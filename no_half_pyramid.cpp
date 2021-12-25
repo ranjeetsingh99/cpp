42ac8bbb6d20d5387be5c4fd47cb652237eb44cc
ranjeetsingh99 <rs116110@gmail.com>
2021-12-25 19:58:30 +0530

    .

--- no_half_pyramid.cpp
+++ no_half_pyramid.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int no,m=1;
+    int no;
     cout<<"Enter a number : ";
     cin>>no;
     for(int i=0;i<no;i++){
         for(int j=0;j<=i;j++)
-            printf("%-2d",m);
+            printf("%-2d",i+1);
         cout<<endl;
-        m++;
+        
     }
     return 0;
 }

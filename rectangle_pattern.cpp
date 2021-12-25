2dc778663420db4d647cde9b2bf5ed1c6fe1e52a
ranjeetsingh99 <rs116110@gmail.com>
2021-12-25 17:41:26 +0530

    retangle pattern

--- rectangle_pattern.cpp
+++ rectangle_pattern.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 int main(){
     int row,col;
+    char ch;
     cout<<"Enter the Number of rows and columns : ";
     cin>>row>>col;
+    cout<<"Enter a Character : ";
+    cin>>ch;
     for (int i=0;i<row;i++) {
         for(int j=0;j<col;j++)
-            cout<<"*";
+            cout<<ch;
         cout<<endl;
     }
     return 0;

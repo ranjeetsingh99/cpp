2ab307e52381490b8c95ac7597f2651a1035924c
ranjeetsingh99 <rs116110@gmail.com>
2021-12-27 10:38:23 +0530

    number pyramid

---
+++ number_pyramid.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int no;
+    cout<<"Enter a number : ";
+    cin>>no;
+
+    for(int i=0;i<no;i++){
+        for(int j=1;j<no-i;j++)
+            cout<<" ";
+        
+        for(int j=0;j<=i;j++){
+            cout<<j+1<<" ";
+        }
+
+        cout<<endl;
+    }
+    return 0;
+}

6d87d192bd0d142b02343ad3a1062c1ac75dfe6d
ranjeetsingh99 <rs116110@gmail.com>
2021-12-28 08:52:33 +0530

    pythagoreon triplets

---
+++ functions/pythagoreon_triplets.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+bool check(int a,int b,int c){
+    int x=max(a,max(b,c));
+    int y,z;
+    if(x==a){ 
+        y=b;
+        z=c;
+    }
+    else if(x==b){ 
+        y=a;
+        z=c;
+    }
+    else {
+        y=a;
+        z=b;
+    }
+    
+    if(x*x==y*y+z*z)    
+        return true;
+    else    
+        return false;
+}
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    
+    if(check(a,b,c))
+        cout<<"Pythagorean Triplet.";
+    else    
+        cout<<"Not Pythagorean Triplet.";
+    
+    return 0;
+}

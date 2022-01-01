d4dbc6c536db6d0b8f5ddb38cea9a837ea787857
ranjeetsingh99 <rs116110@gmail.com>
2022-01-01 21:22:04 +0530

    selection sort

---
+++ functions/array/selection_sort.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of Array : ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+
+    for(int i=0;i<n-1;i++)
+        for(int j=i+1;j<n;j++)
+            if(arr[j]<arr[i]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<"     ";
+    cout<<endl;
+    return 0;
+}

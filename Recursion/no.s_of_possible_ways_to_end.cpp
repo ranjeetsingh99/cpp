8079227464ff81a593ccb4c1976e7a83b0d2ca92
ranjeetsingh99 <rs116110@gmail.com>
2022-02-03 16:51:55 +0530

    recursion

---
+++ Recursion/no.s_of_possible_ways_to_end.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+unsigned long long count(int n,int m){
+
+    if(n==1 || m==1) {  
+        
+        return 1;
+    }
+    return count(n-1,m)+count(n,m-1);
+}
+int main(){
+    int n,m; 
+    cin>>n>>m;
+    cout<<count(n,m);
+
+
+    return 0;
+}

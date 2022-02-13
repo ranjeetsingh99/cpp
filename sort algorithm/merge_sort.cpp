805fdf584e8bda3bc530055eb88153aa118d6be7
ranjeetsingh99 <rs116110@gmail.com>
2022-02-13 16:38:00 +0530

    .

--- sort algorithm/merge_sort.cpp
+++ sort algorithm/merge_sort.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
-
-void print(int a[],int n){
-    for(int i=0;i<n;i++)
+void print_array(int a[],int n){
+    for(int i=0;i<n;i++){
         cout<<a[i]<<" ";
+    }
+    cout<<endl;
 }
 
 void merge(int a[],int left,int mid,int right){
     int temp[right];
     int i=left,j=mid+1,k=left;
 
-    while(i<=mid &&j<=right){
-        if(a[i]<=a[j])
+    while(i<=mid&&j<=right){
+        if(a[i]<a[j])
             temp[k++]=a[i++];
+
         else
             temp[k++]=a[j++];
     }
@@ -21,12 +22,14 @@ void merge(int a[],int left,int mid,int right){
     if(i>mid)
         while(j<=right)
             temp[k++]=a[j++];
+
     else
         while(i<=mid)
             temp[k++]=a[i++];
     
     for(i=left;i<=right;i++)
         a[i]=temp[i];
+
 }
 
 void merge_sort(int a[],int left,int right){
@@ -36,20 +39,18 @@ void merge_sort(int a[],int left,int right){
         merge_sort(a,mid+1,right);
 
         merge(a,left,mid,right);
-        
     }
 }
+
 int main(){
     int n;
-
     cin>>n;
     int a[n];
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++)
         cin>>a[i];
-    }
+
     merge_sort(a,0,n-1);
 
-    print(a,n);
-
+    print_array(a,n);
     return 0;
 }

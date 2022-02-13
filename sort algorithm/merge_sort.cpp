155ac374837864a8604ac3038f80ad94cc9ae04d
ranjeetsingh99 <rs116110@gmail.com>
2022-02-13 16:38:20 +0530

    .

--- sort algorithm/merge_sort.cpp
+++ sort algorithm/merge_sort.cpp
@@ -1,56 +1,56 @@
 #include<iostream>
 using namespace std;
-void print_array(int a[],int n){
-    for(int i=0;i<n;i++){
-        cout<<a[i]<<" ";
+void print_array(int a[] , int n) {
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void merge(int a[],int left,int mid,int right){
+void merge(int a[] , int left , int mid , int right) {
     int temp[right];
-    int i=left,j=mid+1,k=left;
+    int i = left , j = mid + 1 , k = left;
 
-    while(i<=mid&&j<=right){
-        if(a[i]<a[j])
-            temp[k++]=a[i++];
+    while (i <= mid && j <= right) {
+        if (a[i] < a[j])
+            temp[k++] = a[i++];
 
         else
-            temp[k++]=a[j++];
+            temp[k++] = a[j++];
     }
 
-    if(i>mid)
-        while(j<=right)
-            temp[k++]=a[j++];
+    if (i > mid)
+        while (j <= right)
+            temp[k++] = a[j++];
 
     else
-        while(i<=mid)
-            temp[k++]=a[i++];
-    
-    for(i=left;i<=right;i++)
-        a[i]=temp[i];
+        while (i <= mid)
+            temp[k++] = a[i++];
+
+    for (i = left; i <= right; i++)
+        a[i] = temp[i];
 
 }
 
-void merge_sort(int a[],int left,int right){
-    if(left<right){
-        int mid=(left+right)/2;
-        merge_sort(a,left,mid);
-        merge_sort(a,mid+1,right);
+void merge_sort(int a[] , int left , int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        merge_sort(a , left , mid);
+        merge_sort(a , mid + 1 , right);
 
-        merge(a,left,mid,right);
+        merge(a , left , mid , right);
     }
 }
 
-int main(){
+int main() {
     int n;
-    cin>>n;
+    cin >> n;
     int a[n];
-    for(int i=0;i<n;i++)
-        cin>>a[i];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
 
-    merge_sort(a,0,n-1);
+    merge_sort(a , 0 , n - 1);
 
-    print_array(a,n);
+    print_array(a , n);
     return 0;
 }
